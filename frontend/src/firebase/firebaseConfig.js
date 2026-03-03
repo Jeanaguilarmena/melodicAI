1b930a1c502f4e38b9326c8d4c65f2739df493d5
Jean Freddy Aguilar Mena <169120118+Jeanaguilarmena@users.noreply.github.com>
2026-03-03 12:41:52 -0600

    Change apiKey for security reasons

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+//Firebase frontend config with enviroment variables
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
